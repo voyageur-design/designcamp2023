b161f7e4e014e46c0cfd4544ea7636d96d6bdac7
voyageur-design <132100375+voyageur-design@users.noreply.github.com>
2023-08-01 18:37:10 -0500

    update code

--- designcamp-anim.js
+++ designcamp-anim.js
@@ -1,107 +1,126 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
-    let typeSplit = new SplitType("[text-split]", {
-        types: "words, chars",
-        tagName: "span"
-    });
-    let tload = gsap.timeline();
-    tload.to("#preload-animation", {
-        delay: 2,
-        duration: 0.2,
-        scale: 0,
+  let typeSplit = new SplitType("[text-split]", {
+    types: "words, chars",
+    tagName: "span",
+  });
+  let tload = gsap.timeline();
+  tload
+    .to("#preload-animation", {
+      delay: 2,
+      duration: 0.2,
+      scale: 0,
     })
-        .to("#preloader", {
-            height: 0,
-            duration: .5,
-            ease: "expo.inOut",
-        })
-        .to("#preloader", {
-            display: "none",
-        })
-        .from($("[words-slide-up]").find(".word"), { 
-            opacity: 0, 
-            yPercent: 100, 
-            duration: 0.2, 
-            ease: "back.out(2)", 
-            stagger: { amount: 0.3},
-            }, ">0.01")
-        .from("[scale-in]", {
-            duration: 0.5,
-            opacity: 0,
-            scale: 0,
-            ease: "expo.out",
-        }, ">0")
-        .from("[hero-stagger]", {
-            duration: 0.2,
-            opacity: 0,
-            yPercent: -100,
-            stagger: { amount: 0.3},
-            ease: "expo.inOut",
-        }, ">0.2")
-        .from(".logo_banner", {
-            duration: 0.3,
-            opacity: 0,
-            yPercent: -110,
-            ease: "expo.inOut",
-        }, ">")
-});
-
-gsap.from("#location", {
-    scale: 0.5,
-    opacity: 0,
-    borderRadius: "100rem",
-    ease: "expo.inOut",
-    scrollTrigger: {
-        trigger: ".location_section",
-        start: 'top bottom',
-        end: 'top top',
-        scrub: true,
+    .to("#preloader", {
+      height: 0,
+      duration: 0.5,
+      ease: "expo.inOut",
+    })
+    .to("#preloader", {
+      display: "none",
+    })
+    .from(
+      $("[words-slide-up]").find(".word"),
+      {
+        opacity: 0,
+        yPercent: 100,
+        duration: 0.2,
+        ease: "back.out(2)",
+        stagger: { amount: 0.3 },
       },
+      ">0.01"
+    )
+    .from(
+      "[scale-in]",
+      {
+        duration: 0.5,
+        opacity: 0,
+        scale: 0,
+        ease: "expo.out",
+      },
+      ">0"
+    )
+    .from(
+      "[hero-stagger]",
+      {
+        duration: 0.2,
+        opacity: 0,
+        yPercent: -100,
+        stagger: { amount: 0.3 },
+        ease: "expo.inOut",
+      },
+      ">0.2"
+    )
 });
 // Avoid flash of unstyled content
 gsap.set("[text-split]", { opacity: 1 });
 
-let scrollTl = gsap.timeline({})
+let scrollTl = gsap.timeline({});
 
 $(document).ready(function () {
-    scrollTl.to(".logo_banner", {
-        width: "8rem",
-        ease: "expo.inOut",
-        scrollTrigger: {
-            trigger: ".location_section",
-            start: 'top bottom',
-            scrub: true,
-          },
-    },)
-    .from(".header", {
-        height: "0rem",
-        ease: "expo.inOut",
-        duration: 0.2,
-        scrollTrigger: {
-            trigger: ".hero",
-            start: 'center bottom',
-            scrub: true,
-          },
+  scrollTl
+    .to("[banner]", {
+      width: "8rem",
+      scrollTrigger: {
+        trigger: ".location_section",
+        start: "bottom bottom",
+        scrub: true,
+      },
     })
-    .from(".header-container", {
-        display: "none",
-        scrollTrigger: {
-            trigger: ".hero",
-            start: 'center bottom',
-            scrub: true,
-          },
-    })
-    .from(".header-container", {
-        delay: 0.05,
-        opacity: 0,
-        yPercent: -100,
-        scrollTrigger: {
-            trigger: ".hero",
-            start: 'bottom bottom',
-            scrub: true,
-          },
-    }, ">")
 });
+console.clear();
+
+const can = $("[bob]");
+
+const randomX = random(10, 15);
+const randomY = random(10, 15);
+const randomDelay = random(0, 3);
+const randomTime = random(3, 5);
+const randomTime2 = random(5, 10);
+const randomAngle = random(8, 12);
+
+TweenLite.set(can, {
+  x: randomX(-1),
+  y: randomX(1),
+  rotation: randomAngle(-1),
+});
+
+moveX(can, 1);
+moveY(can, -1);
+rotate(can, 1);
+
+function rotate(target, direction) {
+  TweenLite.to(target, randomTime2(), {
+    rotation: randomAngle(direction),
+     delay: randomDelay(),
+    ease: Sine.easeInOut,
+    onComplete: rotate,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function moveX(target, direction) {
+  TweenLite.to(target, randomTime(), {
+    x: random([0, 100]),
+    ease: Sine.easeInOut,
+    onComplete: moveX,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function moveY(target, direction) {
+  TweenLite.to(target, randomTime(), {
+    y: randomY(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveY,
+    onCompleteParams: [target, direction * -1],
+  });
+}
+
+function random(min, max) {
+  const delta = max - min;
+  return (direction = 1) => (min + delta * Math.random()) * direction;
+}
 //On Mouse Hover, change color
 // $('[mouse-hover]').hover(function () {
 //     gsap.to(this, {
@@ -115,3 +134,82 @@ $(document).ready(function () {
 //         }
 //     })
 // });
+ScrollTrigger.matchMedia({
+  "(min-width: 991px)": function () {
+    let tlB = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".hero",
+          start: "top 0%",
+          end: "bottom 15%",
+          toggleActions: "restart none none reverse",
+          markers: false,
+          scrub: 1,
+        },
+      })
+
+      .to("[minnesota]", { y: "10%" }, 0)
+      .to("[top-block]", { y: "5svh" }, 0)
+      .to("[mid-block]", { y: "6svh" }, 0)
+      .to("[bot-block]", { y: "5svh" }, 0)
+      .to("[bob]", { y: "-1svh" }, 0);
+  },
+
+  // TABLET AND BELOW ANIMATION
+  "(max-width: 991px)": function () {
+    let tlB = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".hero",
+          start: "top 0%",
+          end: "bottom 15%",
+          toggleActions: "restart none none reverse",
+          markers: false,
+          scrub: 1,
+        },
+      })
+
+      .to("[minnesota]", { y: "10%" }, 0)
+      .to("[top-block]", { y: "5svh" }, 0)
+      .to("[mid-block]", { y: "6svh" }, 0)
+      .to("[bot-block]", { y: "5svh" }, 0);
+  },
+});
+$(document).ready(function () {
+  
+
+  $(".tickets-item").each(function (index) {
+    let ticketItem = $(this);
+    let fillSvg = $(this).find(".fill-svg");
+    let tixImg = $(this).find(".tiximage_wrap");
+    let tixBadge = $(this).find(".tix_badge");
+    
+      gsap.matchMedia().add("(min-width: 992px)", () => {
+        
+        let tlTix = gsap.timeline({ paused: true, defaults: { duration: 0.2 } });
+        
+        tlTix.fromTo(ticketItem, 
+        { 
+          top: "6rem",
+          color: "#d3ea8e",
+          backgroundColor: "#1a3c35",
+          duration: 0.3,
+          ease: "expo.inOut",
+        }, { 
+          top: "0rem",
+          color: "#193c35",
+          backgroundColor: "#eee6d6", 
+        }, "<")
+        tlTix.fromTo(fillSvg, { fill: "" }, { fill: "currentColor", color: "#b6ad9c" }, "<");
+        tlTix.fromTo(tixImg, { opacity: 0, duration: 0.3, }, { opacity: 1, }, "<");
+        tlTix.fromTo(tixBadge, { opacity: 0, duration: 0.3, }, { opacity: 1, }, "<");
+
+        $(this).on("mouseenter", function () {
+          tlTix.timeScale(1);
+          tlTix.play();
+        });
+        $(this).on("mouseleave", function () {
+          tlTix.timeScale(2);
+          tlTix.reverse();
+        });
+      });
+  });
+});
--- designcamp-marquee.js
+++ designcamp-marquee.js
@@ -1,58 +1,59 @@
 const init = () => {
-  const marquee = document.querySelector('[wb-data="marquee"]');
+  const marquee = document.querySelectorAll('[dc-data="marquee"]');
   if (!marquee) {
     return;
   }
-  const duration = parseInt(marquee.getAttribute("duration"), 10) || 5;
-  console.log(duration);
-  const marqueeContent = document.querySelector(".marquee-content");
-  if (!marqueeContent) {
-    return;
-  }
 
-  const marqueeContentClone = marqueeContent.cloneNode(true);
-  marquee.append(marqueeContentClone);
+  marquee.forEach((marquee) => {
+    const duration = parseInt(marquee.getAttribute("duration"), 10) || 5;
+    const marqueeContent = marquee.firstChild;
+    if (!marqueeContent) {
+      return;
+    }
 
-  let tween;
+    const marqueeContentClone = marqueeContent.cloneNode(true);
+    marquee.append(marqueeContentClone);
 
-  const playMarquee = () => {
-    let progress = tween ? tween.progress() : 0;
-    tween && tween.progress(0).kill();
-    const width = parseInt(
-      getComputedStyle(marqueeContent).getPropertyValue("width"),
-      10
-    );
+    let tween;
 
-    const gap = parseInt(
-      getComputedStyle(marqueeContent).getPropertyValue("column-gap"),
-      10
-    );
-
-    const distanceToTranslate = -1 * (gap + width);
-
-    tween = gsap.fromTo(
-      marquee.children,
-      { x: 0 },
-      { x: distanceToTranslate, duration, ease: "none", repeat: -1 }
-    );
-    tween.progress(progress);
-  };
-  playMarquee();
-
-  function debounce(func) {
-    var timer;
-    return function (e) {
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(
-        () => {
-          func();
-        },
-        500,
-        e
+    const playMarquee = () => {
+      let progress = tween ? tween.progress() : 0;
+      tween && tween.progress(0).kill();
+      const width = parseInt(
+        getComputedStyle(marqueeContent).getPropertyValue("width"),
+        10
       );
-    };
-  }
+      const gap = parseInt(
+        getComputedStyle(marqueeContent).getPropertyValue("column-gap"),
+        10
+      );
+      const distanceToTranslate = -1 * (gap + width);
 
-  window.addEventListener("resize", debounce(playMarquee));
+      tween = gsap.fromTo(
+        marquee.children,
+        { x: 0 },
+        { x: distanceToTranslate, duration, ease: "none", repeat: -1 }
+      );
+      tween.progress(progress);
+      console.log({ width });
+    };
+    playMarquee();
+
+    function debounce(func) {
+      var timer;
+      return function (event) {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(
+          () => {
+            func();
+          },
+          500,
+          event
+        );
+      };
+    }
+
+    window.addEventListener("resize", debounce(playMarquee));
+  });
 };
 document.addEventListener("DOMContentLoaded", init);
